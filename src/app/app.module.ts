94a323203c336933413dcbb2c28b179808e5f67c
Maciek <maciej.bolsega@gmail.com>
2020-04-17 21:03:58 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipieDetailComponent } from './recipies/recipie-detail/recipie-detail.component'
+import { RecipieListComponent } from './recipies/recipie-list/recipie-list.component'
+import { RecipieItemComponent } from './recipies/recipie-list/recipie-item/recipie-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipieDetailComponent,
+    RecipieListComponent,
+    RecipieItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
