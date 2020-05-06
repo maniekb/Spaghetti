33b8225b7924d3bbcdabd5e3844b977488ac9cf6
Maciek <maciej.bolsega@gmail.com>
2020-05-07 00:57:01 +0200

    shopping list service

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,19 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
+
+export class ShoppingListService {
+    ingredientsUpdated = new EventEmitter<Ingredient[]>();
+    private ingredients: Ingredient[] = [
+        new Ingredient("Tomato", 2),
+        new Ingredient("Pasta", 100)
+      ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    }
+
+    addIngredient(newIngredient: Ingredient) {
+        this.ingredients.push(newIngredient);
+        this.ingredientsUpdated.emit(this.ingredients.slice());
+    }
+}
